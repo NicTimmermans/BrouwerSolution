d1135dfeba6f5673642f9bfbd88e25d9a06d4e66
Nic TIMMERMANS <Nic.TIMMERMANS@vdabcampus.be>
2020-11-06 18:13:36 +0100

    Get request

--- BrouwerService/Controllers/BrouwerController.cs
+++ BrouwerService/Controllers/BrouwerController.cs
@@ -15,5 +15,19 @@ namespace BrouwerService.Controllers
         private readonly IBrouwerRepository repository;
         public BrouwerController(IBrouwerRepository repository) =>
          this.repository = repository;
+        [HttpGet]
+        public ActionResult FindAll() => base.Ok(repository.FindAll());
+        [HttpGet("{id}")]
+        public ActionResult FindById(int id)
+        {
+            var brouwer = repository.FindById(id);
+            if (brouwer == null)
+            {
+                return base.NotFound();
+            }
+            return base.Ok(brouwer);
+        }
+        [HttpGet("naam")]
+        public ActionResult FindByBeginNaam(string begin) => base.Ok(repository.FindByBeginNaam(begin));
     }
 }
